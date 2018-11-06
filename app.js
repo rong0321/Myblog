fb3d25b074fba3eb3133dbf43a6d75f96a5798db
rx <1003142361@qq.com>
2018-11-06 08:50:38 +0800

    注册页面及功能完成

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views','./views')
 
+//引入body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+
+//引入mysql
+const mysql = require('mysql')
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'time_blog'
+})
+
+//引入moment
+const moment = require('moment')
+
+
 //托管node_modules目录
 app.use('/node_modules',express.static('./node_modules'))
 
@@ -11,10 +28,38 @@ app.use('/node_modules',express.static('./node_modules'))
 
 app.get('/',(req,res) => {
     //render方法必须要先配置模板引擎,此处相对于设置的模板引擎的目录
-    res.render('./index.ejs',{
-        name:'zs',
-        age:18
+    res.render('./index.ejs',{ })
+})
+
+app.get('/register',(req,res) => {
+    res.render('./user/register.ejs',{})
+})
+app.get('/login',(req,res) => {
+    res.render('./user/login.ejs',{})
+})
+
+app.post('/register',(req,res) => {
+    // console.log('123')
+    // console.log(req.body);
+    const user = req.body
+    if(user.username.trim().length == 0 
+    || user.password.trim().length == 0 
+    || user.nickname.trim().length == 0
+    ) return res.status(400).send({status:400,msg:'请填写完整的表单信息'})
+    const sql1 = "select * from users where  username = ?"
+    conn.query(sql1,user.username,(err,result) => {
+        if(err) return res.status(401).send({status:401,msg:'用户名查重失败'})
+        // console.log(result);
+        if(result.length != 0) return res.send({status:402,msg:'用户名重复'})
+        const sql2 = "insert into users set ?"
+        user.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+        conn.query(sql2,user,(err,result) => {
+            if(err || result.affectedRows != 1) return res.status(500).send({status:500,msg:'添加用户失败'})
+            res.send({status:200,msg:'注册用户成功'})
+        })
     })
+
+    // res.send({status:200,msg:'注册用户成功! '})
 })
 
 
