c340a7ea20bc2ff68f7122673e8254db26aa48a5
rx <1003142361@qq.com>
2018-11-08 20:48:24 +0800

    完成文章详情页动态渲染,完成文章编辑功能

--- controller/article.js
+++ controller/article.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 const conn = require('../db/db.js')
-
+const marked = require('marked')
 
 
 module.exports = {
@@ -22,15 +22,11 @@ module.exports = {
         // console.log(body)
         const insertSql = 'insert into articles set ?'
         conn.query(insertSql, body, (err, result) => {
-            if (err) return res.status(500).send({
+            if (err || result.affectedRows != 1) return res.status(500).send({
                 status: 500,
                 msg: '添加文章失败!请重试'
             })
             // console.log(result)
-            if (result.affectedRows != 1) return res.status(500).send({
-                status: 500,
-                msg: '添加文章失败!请重试'
-            })
             res.send({
                 status: 200,
                 msg: 'ok',
@@ -39,9 +35,49 @@ module.exports = {
         })
     },
     handleGetArticleInfo(req, res) {
-        res.render('./article/info.ejs', {
-            user: req.session.user,
-            isLogin: req.session.isLogin
+        const id = req.params.id
+        // console.log(id)
+        const querySql = "select * from articles where id = ?"
+        conn.query(querySql, id, (err, result) => {
+            if (err) return res.status(500).send({
+                status: 500,
+                msg: '获取文章详情失败',
+                data: null
+            })
+            if (result.length != 1) return res.redirect('/')
+            const html = marked(result[0].content)
+            // console.log(html)
+            result[0].content = html
+            res.render('./article/info.ejs', {
+                user: req.session.user,
+                isLogin: req.session.isLogin,
+                article: result[0]
+            })
+        })
+
+    },
+    handleGetArticleEdit(req, res) {
+        if (!req.session.isLogin) return res.redirect('/')
+        const articleId = req.params.id
+        const sql = "select * from articles where id = ?"
+        conn.query(sql, articleId, (err, result) => {
+            if (err || result.length != 1) return res.redirect('/')
+            // console.log(result)
+            res.render('./article/edit.ejs', {
+                user: req.session.user,
+                isLogin: req.session.isLogin,
+                article: result[0]
+            })
+        })
+
+
+    },
+    handlePostArticleEdit(req, res) {
+        const body = req.body
+        const sql = "update articles set ? where id =?"
+        conn.query(sql,[body,body.id],(err,result) => {
+            if(err || result.affectedRows != 1) return res.status(500).send({status:500,msg:'编辑文章失败!请重试'})
+            res.send({status:200,msg:'ok'})
         })
     }
 }
