584c17f237b4e8087299111bd974438234831cf1
rx <1003142361@qq.com>
2018-11-09 19:15:35 +0800

    完成sql文件抽离

---
+++ docs/time_blog.sql
@@ -0,0 +1,43 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2018-11-09 18:45:00
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */
+
+
+#
+# Database "time_blog"
+#
+
+CREATE DATABASE IF NOT EXISTS `time_blog` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
+USE `time_blog`;
+
+#
+# Structure for table "articles"
+#
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text,
+  `ctime` varchar(255) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `ctime` varchar(255) NOT NULL,
+  `isdel` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为删除 0为未删除',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
